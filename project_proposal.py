c4465f9b07b93346a365a3787d867339fbf69801
dylanc9918 <93285562+dylanc9918@users.noreply.github.com>
2023-03-05 02:51:26 +0000

    data wrangled some of the reading of data to make it cleaner

--- project_proposal.py
+++ project_proposal.py
@@ -5,17 +5,23 @@ import numpy as np
 import glob
 import os
 
-path = r'data/'
+path_to_folder = r'data/'
 
 csv_files = ["YVRStationData_2013-2015.csv", "YVRStationData_2015-2017.csv",
              "YVRStationData_2017-2019.csv", "YVRStationData_2019-2021.csv", "YVRStationData_2021-2023.csv"]
 
 df = pd.DataFrame()
 
+# read in data files separatly and append to eachother
 for file in csv_files:
-    df_temp = pd.read_csv(path+file)
+    df_temp = pd.read_csv(path_to_folder+file, header=[0, 1])
     df = df.append(df_temp, ignore_index=True)
 
-df.columns = df.columns.str.strip()
+# join the gases and their units together via an underscore
+df.columns = df.columns.map('_'.join)
 
-df["TEMP_MEAN"].head()
+# replace whitespace within columns
+df.columns = df.columns.str.replace(' ', '')
+
+# renamed to date to make it easier to index
+df = df.rename(columns={"DateTime_": "date"})
