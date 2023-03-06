efbbb824409e5cf6caae7eb517773c2d620cdb29
dylanc9918 <93285562+dylanc9918@users.noreply.github.com>
2023-03-06 20:38:34 +0000

    Added code to group the values by month and find averages. Plotted that as subplots and removed uneccessary columns

--- project_proposal.py
+++ project_proposal.py
@@ -36,47 +36,48 @@ for col in cols:
     df[col] = pd.to_numeric(df[col], errors='coerce')
 
 
-# Remove empty Station column
-df = df.drop(columns=["Station:VancouverInternationalAirport#2-_"])
+# Remove empty Station column and other unecessary columns
+df = df.drop(columns=["Station:VancouverInternationalAirport#2-_", "WSPD_SCLR_m/s", 'WDIR_SCLR_Deg.',
+             'WSPD_VECT_m/s', 'WDIR_VECT_Deg.', 'ATM_PRESS_1HR_kPa', 'HUMIDITY_%RH', 'RAD_TOTAL_W/M**2'])
 
 
 # Formats the date column from a string to datetime column in pandas
 df['date'] = pd.to_datetime(df['date'], format=" %m/%d/%Y %H%M:%S %p ")
 
 
-# creates a plot of the monthly averages for the CO_ppm column
-CO_monthly_avg = df.groupby(pd.PeriodIndex(
-    df['date'], freq="M"))['CO_ppm'].mean()
-CO_monthly_avg.plot()
+# creates a plot of the monthly averages for the all columns
+monthly_avg = df.groupby(pd.PeriodIndex(
+    df['date'], freq="M")).mean()
+
+monthly_avg.plot(subplots=True, layout=(
+    5, 2), grid=True, sharex=True, figsize=(15, 8))
+plt.tight_layout()
+
+# fig, ax = plt.subplots(3, 2, sharex=True)
+# fig.tight_layout(h_pad=2)
 
 
-fig, ax = plt.subplots(3, 2, sharex=True)
-fig.tight_layout(h_pad=2)
+# ax[0, 0].plot(monthly_avg["CO_ppm"])
+# ax[0, 1].plot(df['date'], df["NO_ppb"])
+# ax[1, 0].plot(df['date'], df["NO2_ppb"])
+# ax[1, 1].plot(df['date'], df["O3_ppb"])
+# ax[2, 0].plot(df['date'], df["SO2_ppb"])
+# ax[2, 1].plot(df['date'], df["PM10_ug/m3"])
 
 
-ax[0, 0].CO_monthly_avg.plot()
-ax[0, 1].plot(df['date'], df["NO_ppb"])
-ax[1, 0].plot(df['date'], df["NO2_ppb"])
-ax[1, 1].plot(df['date'], df["O3_ppb"])
-ax[2, 0].plot(df['date'], df["SO2_ppb"])
-ax[2, 1].plot(df['date'], df["PM10_ug/m3"])
+# ax[0, 0].set_title("CO Levels")
+# ax[0, 1].set_title("NO Levels")
+# ax[1, 0].set_title("NO2 Levels")
+# ax[1, 1].set_title("O3 Levels")
+# ax[2, 0].set_title("SO2 Levels")
+# ax[2, 1].set_title("PM10_ug/m3 Levels")
 
 
-ax[0, 0].set_title("CO Levels")
-ax[0, 1].set_title("NO Levels")
-ax[1, 0].set_title("NO2 Levels")
-ax[1, 1].set_title("O3 Levels")
-ax[2, 0].set_title("SO2 Levels")
-ax[2, 1].set_title("PM10_ug/m3 Levels")
+# dtFmt = mdates.DateFormatter('%Y')  # define the formatting
+# # apply the format to the desired axis
+# plt.gca().xaxis.set_major_formatter(dtFmt)
 
 
-dtFmt = mdates.DateFormatter('%Y')  # define the formatting
-# apply the format to the desired axis
-plt.gca().xaxis.set_major_formatter(dtFmt)
-
-
-plt.ylabel("Concentration (ppm)")
-plt.xlabel("Date")
-plt.show()
-
-
+# plt.ylabel("Concentration (ppm)")
+# plt.xlabel("Date")
+# plt.show()
