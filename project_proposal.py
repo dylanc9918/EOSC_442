8d1247080b24d96f99b3494861fb1851b0cd340a
dylanc9918 <93285562+dylanc9918@users.noreply.github.com>
2023-03-06 17:08:59 +0000

    added code to plot all the columns of interest created subplots with titles

--- project_proposal.py
+++ project_proposal.py
@@ -30,16 +30,55 @@ df.columns = df.columns.str.replace(' ', '')
 # renamed to date to make it easier to index
 df = df.rename(columns={"DateTime_": "date"})
 
+# change all columns to numeric except for date
+cols = [i for i in df.columns if i not in ["date"]]
+for col in cols:
+    df[col] = pd.to_numeric(df[col], errors='coerce')
+
+
+# Remove empty Station column
+df = df.drop(columns=["Station:VancouverInternationalAirport#2-_"])
+
 
 # Formats the date column from a string to datetime column in pandas
 df['date'] = pd.to_datetime(df['date'], format=" %m/%d/%Y %H%M:%S %p ")
 
-plt.scatter(df["date"], df["CO_ppm"])
+
+# creates a plot of the monthly averages for the CO_ppm column
+CO_monthly_avg = df.groupby(pd.PeriodIndex(
+    df['date'], freq="M"))['CO_ppm'].mean()
+CO_monthly_avg.plot()
+
+
+fig, ax = plt.subplots(3, 2, sharex=True)
+fig.tight_layout(h_pad=2)
+
+
+ax[0, 0].CO_monthly_avg.plot()
+ax[0, 1].plot(df['date'], df["NO_ppb"])
+ax[1, 0].plot(df['date'], df["NO2_ppb"])
+ax[1, 1].plot(df['date'], df["O3_ppb"])
+ax[2, 0].plot(df['date'], df["SO2_ppb"])
+ax[2, 1].plot(df['date'], df["PM10_ug/m3"])
+
+
+ax[0, 0].set_title("CO Levels")
+ax[0, 1].set_title("NO Levels")
+ax[1, 0].set_title("NO2 Levels")
+ax[1, 1].set_title("O3 Levels")
+ax[2, 0].set_title("SO2 Levels")
+ax[2, 1].set_title("PM10_ug/m3 Levels")
+
+
 dtFmt = mdates.DateFormatter('%Y')  # define the formatting
 # apply the format to the desired axis
 plt.gca().xaxis.set_major_formatter(dtFmt)
-plt.gca().yaxis.set_major_locator(MaxNLocator(prune='lower'))
+
 
 plt.ylabel("Concentration (ppm)")
 plt.xlabel("Date")
 plt.show()
+
+
+# create an average of each month for CO levels
+plt.plot(df.groupby(df.date.dt.month)['CO_ppm'].mean())
