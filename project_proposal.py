3b149187d818ea48ca1734c1d759d98950592267
dylanc9918 <dylanc9918@gmail.com>
2023-03-31 11:39:57 -0700

    fixed code so line now properly plots data points

--- project_proposal.py
+++ project_proposal.py
@@ -56,70 +56,70 @@ df['date'] = pd.to_datetime(df['date'], format=" %m/%d/%Y %H%M:%S %p ")
 
 
 # creates a plot of the monthly averages for the all columns
-monthly_avg = df.groupby(pd.PeriodIndex(
-    df['date'], freq="M")).mean()
+# monthly_avg = df.groupby(pd.PeriodIndex(
+#     df['date'], freq="M")).mean()
 
 
-fig1, ax1 = plt.subplots(len(monthly_avg.columns),
-                         figsize=[25, 15], sharex=True)
-fig1.tight_layout(pad=5)
-fig1.suptitle("Monthly Averages")
+# fig1, ax1 = plt.subplots(len(monthly_avg.columns),
+#                          figsize=[25, 15], sharex=True)
+# fig1.tight_layout(pad=5)
+# fig1.suptitle("Monthly Averages")
 
-titles = ["CO", "NO2", "O3", "PM 2.5", "PM 10",
-          "Temperature", "Precipitation Total"]
+# titles = ["CO", "NO2", "O3", "PM 2.5", "PM 10",
+#           "Temperature", "Precipitation Total"]
 
-units = ["ppm", "ppb", "ppb", "µg/m³", "µg/m³", "Celsius", "mm"]
+# units = ["ppm", "ppb", "ppb", "µg/m³", "µg/m³", "Celsius", "mm"]
 
-colors = sns.color_palette(None, len(monthly_avg.columns))
+# colors = sns.color_palette(None, len(monthly_avg.columns))
 
-# loop through each column of the dataframe
-for i in range(len(monthly_avg.columns)):
-    # activate the subplot
+# # loop through each column of the dataframe
+# for i in range(len(monthly_avg.columns)):
+#     # activate the subplot
 
-    # plot the time-series
-    ax1[i].plot(monthly_avg.index.to_timestamp(),
-                monthly_avg.iloc[:, i], color=colors[i])
+#     # plot the time-series
+#     ax1[i].plot(monthly_avg.index.to_timestamp(),
+#                 monthly_avg.iloc[:, i], color=colors[i])
 
-    # add figure elements
-    ax1[i].set_title(titles[i])
-    ax1[i].set_ylabel(units[i])
-    plt.xlabel("Date")
+#     # add figure elements
+#     ax1[i].set_title(titles[i])
+#     ax1[i].set_ylabel(units[i])
+#     plt.xlabel("Date")
 
 
-year_avg = df.groupby(pd.PeriodIndex(
-    df['date'], freq="Y")).mean()
+# year_avg = df.groupby(pd.PeriodIndex(
+#     df['date'], freq="Y")).mean()
 
 
 # creates plots of Yearly averages
-fig, ax = plt.subplots(len(year_avg.columns), figsize=[25, 15], sharex=True)
-fig.tight_layout(pad=5)
-fig.suptitle("Yearly Averages")
+# fig, ax = plt.subplots(len(year_avg.columns), figsize=[25, 15], sharex=True)
+# fig.tight_layout(pad=5)
+# fig.suptitle("Yearly Averages")
 
 
-titles = ["CO", "NO2", "O3", "PM 2.5", "PM 10",
-          "Temperature", "Precipitation Total"]
+# titles = ["CO", "NO2", "O3", "PM 2.5", "PM 10",
+#           "Temperature", "Precipitation Total"]
 
-units = ["ppm", "ppb", "ppb", "µg/m³", "µg/m³", "Celsius", "mm"]
+# units = ["ppm", "ppb", "ppb", "µg/m³", "µg/m³", "Celsius", "mm"]
 
-colors = sns.color_palette(None, len(year_avg.columns))
+# colors = sns.color_palette(None, len(year_avg.columns))
 
-# loop through each column of the dataframe
-for i in range(len(year_avg.columns)):
-    # activate the subplot
+# # loop through each column of the dataframe
+# for i in range(len(year_avg.columns)):
+#     # activate the subplot
 
-    # plot the time-series
-    ax[i].plot(year_avg.index.to_timestamp(),
-               year_avg.iloc[:, i], color=colors[i])
+#     # plot the time-series
+#     ax[i].plot(year_avg.index.to_timestamp(),
+#                year_avg.iloc[:, i], color=colors[i])
 
-    # add figure elements
-    ax[i].set_title(titles[i])
-    ax[i].set_ylabel(units[i])
-    plt.xlabel("Date")
+#     # add figure elements
+#     ax[i].set_title(titles[i])
+#     ax[i].set_ylabel(units[i])
+#     plt.xlabel("Date")
 
 
-monthly_avg.plot(subplots=True, layout=(
-    5, 2), grid=True, sharex=True, figsize=(15, 8))
-plt.tight_layout()
+# monthly_avg.plot(subplots=True, layout=(
+#     5, 2), grid=True, sharex=True, figsize=(15, 8))
+# plt.tight_layout()
 
 
 # creates season mapping based on months in dataframe
@@ -138,13 +138,6 @@ seasonal_avg.plot(subplots=True, grid=True, sharex=True, figsize=(15, 8))
 plt.tight_layout()
 
 
-# list of  gasses and their respective seasons
-# 0 = Winter (DJF)
-# 0 = Spring (MAM)
-# 0 = Summer (JJA)
-# 0 = Fall (SON)
-
-
 seasonal_avg = seasonal_avg.groupby("season")
 
 # creates list of gasses and their seasons
@@ -156,12 +149,20 @@ season_NO2_list = list(seasonal_avg.NO2_ppb)
 season_temp_list = list(seasonal_avg.temp)
 season_precip_list = list(seasonal_avg.PRECIP_TOTAL_mm)
 
+# reverse order of indices 1 and 2 since they are swithced out of order of winter summer spring
+season_CO_list[1], season_CO_list[2] = season_CO_list[2], season_CO_list[1]
 
-list_gas = [season_CO_list,
-            season_O3_list,
-            season_PM25_list,
-            season_PM10_list,
-            season_NO2_list]
+season_O3_list[1], season_O3_list[2] = season_O3_list[2], season_O3_list[1]
+
+season_PM25_list[1], season_PM25_list[2] = season_PM25_list[2], season_PM25_list[1]
+
+season_PM10_list[1], season_PM10_list[2] = season_PM10_list[2], season_PM10_list[1]
+
+season_NO2_list[1], season_NO2_list[2] = season_NO2_list[2], season_NO2_list[1]
+
+season_temp_list[1], season_temp_list[2] = season_temp_list[2], season_temp_list[1]
+
+season_precip_list[1], season_precip_list[2] = season_precip_list[2], season_precip_list[1]
 
 
 # creates subplots of each season and looks at their monthly distribution of temperature vs concentration for each graph. Creates a linear regression line and in addition reports the R-squared values and the p-values
@@ -169,41 +170,41 @@ list_gas = [season_CO_list,
 list_gas = [season_CO_list,
             season_NO2_list,
             season_O3_list,
-            season_PM25_list,
-            season_PM10_list]
+            ]
 
 
 titles = ["CO", "NO2", "O3", "PM 2.5", "PM 10",
           "Temperature", "Precipitation Total"]
 
+units = ["ppm", "ppb", "ppb"]
 
 for j in range(len(list_gas)):
-    fig.text(0.5, 0.00, "Units", ha='center')
     fig.text(0.00, 0.5, 'Temp (C°)', va='center', rotation='vertical')
     season = ["Winter", "Spring", "Summer", "Fall"]
-    fig, ax = plt.subplots(4)
+    fig, ax = plt.subplots(4, figsize=(8, 15))
     fig.tight_layout(h_pad=2)
     fig.suptitle("Seasonal Correlation Graph between " +
                  titles[j] + " and Temperature", y=1.05)
 
+    fig.text(0.5, 0.00, units[j], ha='center')
+    fig.text(0.00, 0.5, 'Temp (C°)', va='center', rotation='vertical')
+    season = ["Winter", "Spring", "Summer", "Fall"]
+
     for i in range(4):
-        ax[i].scatter(list_gas[j][i][1], season_temp_list[i][1])
+        season_list = list(list_gas[j][i][1])
+        ax[i].scatter(season_list, season_temp_list[i][1])
         ax[i].set_title(season[i])
+        season_list = sm.add_constant(season_list)
         model = sm.OLS(season_temp_list[i][1],
-                       list_gas[j][i][1], missing='drop').fit()
-        y_predict = model.predict(list_gas[j][i][1])
-        rmse = sm.tools.eval_measures.rmse(season_temp_list[i][1], y_predict)
+                       season_list, missing='drop').fit()
+        y_predict = model.predict(season_list)
 
         text = 'R-Squared:{:.4f} \np-Value:{:.2E}'.format(
             model.rsquared, model.pvalues[0])
-        ax[i].plot(list_gas[j][i][1], y_predict,
+        ax[i].plot(season_list[:, 1], y_predict,
                    linestyle="-", color="r")
-        ax[i].annotate(text, xy=(0, 1.1),
+        ax[i].annotate(text, xy=(0.01, .9),
                        xycoords='axes fraction', fontsize=8, color='r')
-
-    fig.text(0.5, 0.00, "CO Concentration ppm", ha='center')
-    fig.text(0.00, 0.5, 'Temp (C°)', va='center', rotation='vertical')
-    season = ["Winter", "Spring", "Summer", "Fall"]
 
 
 # Creates subplots that look at the entire year of data by year
@@ -261,10 +262,8 @@ for j in range(len(gas_name)):
         model = sm.OLS(df_group.get_group(list_yrs[i]).temp,
                        df_group.get_group(list_yrs[i]).iloc[:, j+1], missing='drop').fit()
         y_predict = model.predict(df_group.get_group(list_yrs[i]).iloc[:, j+1])
-        rmse = sm.tools.eval_measures.rmse(
-            df_group.get_group(list_yrs[i]).temp, y_predict)
 
-        text = 'R-Squared:{:.4f} \np-Value:{:.4f}'.format(
+        text = 'R-Squared:{:.4f} \np-Value:{:.2E}'.format(
             model.rsquared, model.pvalues[0])
         ax[i].plot(df_group.get_group(list_yrs[i]).iloc[:, j+1], y_predict,
                    linestyle="-", color="r")
